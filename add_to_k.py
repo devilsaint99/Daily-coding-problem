f155361315ba6452d6af11748254310e3f782240
Devilsaint <anshul.suresh99@gmail.com>
2021-09-17 19:00:49 +0530

    Update add_to_k.py

--- add_to_k.py
+++ add_to_k.py
@@ -1,6 +1,10 @@
 """Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
-For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17."""
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+This code works faster than alternate
+
+"""
 
 def add_to_k(addition,k):
     for item in range(0,len(addition)-1):
@@ -8,4 +12,4 @@ def add_to_k(addition,k):
             if addition[item]+addition[jtem]==k:
                 return True
     return False
-print(add_to_k([13,10,3,4],))
+print(add_to_k([13,10,3,4],7))
