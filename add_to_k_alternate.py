dd1f3509b083050c3f102817e321b807800b41f6
Devilsaint <anshul.suresh99@gmail.com>
2021-09-17 18:50:55 +0530

    Create add_to_k_alternate.py

---
+++ add_to_k_alternate.py
@@ -0,0 +1,8 @@
+def add_to_k(list1, k):
+    for i in list1[:len(list1)-1]:
+        list1.remove(i)
+        if k-i in list1:
+            return 'True'
+    return 'False' 
+
+print(add_to_k([13,2,3,4],7))
